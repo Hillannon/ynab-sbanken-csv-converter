ee4363324ca4967bf8d55901b0b4014a1c1b1408
Steinar Opphus <steinar.opphus@gmail.com>
2019-06-17 22:07:08 +0200

    Updates

---
+++ precache-manifest.1ed80be977c6d1f729e03d567063bb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478311739e6703b4c25cc17e7b5b3e73",
+    "url": "/ynab-sbanken-csv-converter/index.html"
+  },
+  {
+    "revision": "320990b9875eef410132",
+    "url": "/ynab-sbanken-csv-converter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ynab-sbanken-csv-converter/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "320990b9875eef410132",
+    "url": "/ynab-sbanken-csv-converter/static/js/main.94cd2185.chunk.js"
+  },
+  {
+    "revision": "3310bed211c89b5bd019",
+    "url": "/ynab-sbanken-csv-converter/static/js/runtime~main.3750f90a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ynab-sbanken-csv-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
