9460bb2906a47a2e2abd07a16243434cee4ad69b
Steinar Opphus <steinar.opphus@gmail.com>
2019-06-18 07:56:33 +0200

    Added dropzone.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 import './App.css';
 
+function MyDropzone() {
+    const onDrop = useCallback(acceptedFiles => {
+        const reader = new FileReader();
+
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+        reader.onload = () => {
+            // Do whatever you want with the file contents
+            const binaryStr = reader.result;
+            console.log(binaryStr)
+        };
+
+        acceptedFiles.forEach(file => reader.readAsBinaryString(file))
+    }, []);
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <MyDropzone/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
